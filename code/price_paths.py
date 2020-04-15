f4ed502148e19dc5f613eb99150be0f2d86feb4c
LautaroParada <lautaro.parada.opazo@gmail.com>
2020-04-15 01:30:22 -0400

    vra update
    implementation of several price paths to test

--- code/price_paths.py
+++ code/price_paths.py
@@ -2,17 +2,22 @@ import numpy as np
 
 class SimPaths(object):
 
-	def __init__(self, n:int, T:int, s0:float=1):
+	def __init__(self, n:int, T:int, s0:float=1, rd_mu=0, rd_sigma=1):
 
 		self.n = n
 		self.T = T
 		self.s0 = s0
+		self.r0 = self.s0 / 100 # initial interest rate for cir
+		self.dt = 1 / self.T
+		# inputs for the random disturbance for each time step of the processes
+		self.rd_mu = rd_mu
+		self.rd_sigma = rd_sigma
 
 	# -------------------------------
 	# Helper methods
 	# -------------------------------
 
-	def _random_disturbance(self, mu:float, sigma:float, stochastic_volatility:bool):
+	def _random_disturbance(self, stochastic_volatility:bool):
 		"""
 		Sample a random disturbance from either a normal distribution with a 
 	    constant standard deviation (Geometric Brownian Motion model) or from a 
@@ -33,17 +38,15 @@ class SimPaths(object):
 			sigma(float): standard deviation of a normal distribuition
 			stochastic_volatility(boolean): choose if the disturbance is 
 				Homoskedastic or Heteroskedastic variance. 
-
 		Returns
 			a float number, that represents the disturbance in a series of prices
-
 		"""
 		if not stochastic_volatility:
-			return np.random.normal(mu, sigma, 1)
+			return np.random.normal(self.rd_mu, self.rd_sigma, 1)
 
-		sigma_stochastic = max(np.random.normal(sigma, sigma/2, 1), 0.01)
+		sigma_stochastic = max(np.random.normal(self.rd_sigma, self.rd_sigma/2, 1), 0.01)
 
-		return np.random.normal(mu, sigma_stochastic, 1)
+		return np.random.normal(self.rd_mu, sigma_stochastic, 1)
 
 	def ohlc_candles(self, prices:np.array):
 		"""
@@ -69,23 +72,42 @@ class SimPaths(object):
 
 		return ans[:candle_counter]
 
+	def _corr_noise(self, T:int, rho:float):
+		"""
+		A straightforward way to create noise processes with a specified correlation 
+		is through the singular value decomposition (SVD) 
+		see Sauer (2006) for a description).
+		"""
+		# Two correlated brownian motions
+		# generate two uncorrelated Brownian processes
+		z1 = np.array( [ self._random_disturbance(stochastic_volatility=False) for _ in range(T) ] )
+		z2 = np.array( [ self._random_disturbance(stochastic_volatility=False) for _ in range(T) ] )
+
+		_corr1 = np.power( ( 1 + rho ) / 2, 0.5)
+		_corr2 = np.power( ( 1 - rho ) / 2, 0.5)
+
+		# correlating the brownian processes
+		dw1 = (_corr1 * z1 + _corr2 * z2).reshape(-1, 1)
+		dw2 = (_corr1 * z1 - _corr2 * z2).reshape(-1, 1)
+
+		return dw1, dw1
+
 	# -------------------------------
 	# The Brownian Motion Stochastic Process (Wiener Process)
 	# -------------------------------
 
-	def _brownian_continuous(self, mu:float, xt:float, rd:float, dt:float):
+	def _brownian_continuous(self, mu:float, xt:float, rd:float):
 		"""
 		Brownian motion X with the drift parameter µ, and the variance
 		parameter σ^2, and the initial time t = 0 satisfies the following 
 		stochastic differential equation, where W is a standard 
 		Brownian motion
-
 		dX(t) = µdt + σdW(t) , X(0) = 0
 		where µ=0, σ=1 and dW(t)∼N(0, dt)
 		"""
-		return mu * dt + xt + rd * np.sqrt(dt)
+		return mu * self.dt + xt + rd * np.sqrt(self.dt)
 
-	def _brownian_returns(self, x0:float, T:float, mu:float, rd_mu:float, rd_sigma:float, dt:float, stochastic_volatility:bool):
+	def _brownian_returns(self, x0:float, T:float, mu:float, stochastic_volatility:bool):
 		"""
 		"""
 		# preallocate the data
@@ -93,24 +115,21 @@ class SimPaths(object):
 		X[0] = x0
 
 		for t in range(1, T):
-			_rd = self._random_disturbance(mu=rd_mu, sigma=rd_sigma, stochastic_volatility=stochastic_volatility)
-			X[t] = self._brownian_continuous(mu=mu, xt=X[t-1], rd=_rd, dt=dt)
+			_rd = self._random_disturbance(stochastic_volatility=stochastic_volatility)
+			X[t] = self._brownian_continuous(mu=mu, xt=X[t-1], rd=_rd)
 
 		return X
 
-	def brownian_prices(self, mu:float=0, rd_mu:float=0, rd_sigma:float=1, stochastic_volatility:bool=False):
+	def brownian_prices(self, mu:float=0, stochastic_volatility:bool=False):
 		"""
 		"""
-		dt = 1 / self.T
 		_data = np.empty((self.T, self.n))
 
 		if self.n > 1:
 			for i in range(self.n):
-				_data[:, i] = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+				_data[:, i] = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, stochastic_volatility=stochastic_volatility))
 		else:
-			_data = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+			_data = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, stochastic_volatility=stochastic_volatility))
 
 		return _data
 
@@ -118,12 +137,11 @@ class SimPaths(object):
 	# The Geometric Brownian Motion Stochastic Process
 	# -------------------------------
 
-	def gbm_prices(self, mu:float=1, rd_mu:float=0, rd_sigma:float=1, stochastic_volatility:bool=True):
+	def gbm_prices(self, mu:float=1, stochastic_volatility:bool=True):
 		"""
 		GBM says the change in stock price is the stock 
 		price "S" multiplied by the two terms found inside
 		the parenthesis below
-
 		ΔS = S × (μΔt + σϵΔt)
 		
 		The first term is a "drift" and the second term 
@@ -134,20 +152,16 @@ class SimPaths(object):
 		will be the standard deviation "s" multiplied by
 		a random number "e." This is simply a way of 
 		scaling the standard deviation.
-
 		more details at 
 		https://en.wikipedia.org/wiki/Geometric_Brownian_motion
 		"""
-		dt = 1 / self.T
 		_data = np.empty((self.T, self.n))
 
 		if self.n > 1:
 			for i in range(self.n):
-				_data[:, i] = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+				_data[:, i] = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, stochastic_volatility=stochastic_volatility))
 		else:
-			_data = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+			_data = np.exp(self._brownian_returns(x0=np.log(self.s0), T=self.T, mu=mu, stochastic_volatility=stochastic_volatility))
 
 		return _data
 
@@ -162,26 +176,21 @@ class SimPaths(object):
 		Bi distributed as (say) G (could be continuous 
 		or discrete). Suppose that we already have an 
 		algorithm for generating from G
-
 		Algorithm for generating a compound Poisson process 
 		up to a desired time T to get X(T)
-
 		1. t = 0, N = 0, X = 0.
 		2. Generate a U
 		3. t = t + [−(1/λ) ln (U)]. If t > T, then stop.
 		4. Generate B distributed as G.
 		5. Set N = N + 1 and set X = X + B
 		6. Go back to 2.
-
 		source http://www.columbia.edu/~ks20/4703-Sigman/4703-07-Notes-PP-NSPP.pdf
 		page 8
-
 		https://www.statstodo.com/Poisson_Exp.php
 		https://www.lpsm.paris/pageperso/tankov/tankov_voltchkova.pdf
 		pag 5
 		"""
 		t = 0
-		dt = 1 / self.T
 		x_jumps = np.zeros(self.T)
 		_lambda = lam / self.T
 		small_lambda = -(1.0/_lambda)
@@ -189,36 +198,32 @@ class SimPaths(object):
 		for n in range(self.T):
 			t += small_lambda * np.log(np.random.uniform(0,1))
 			if t > self.T:
-				x_jumps[n:] = ( np.random.poisson(_lambda, 1) + np.random.uniform(0, 1) ) * np.random.normal(0, 1)
+				x_jumps[n:] = ( np.random.poisson(_lambda, 1) + np.random.uniform(0, 1) ) * np.random.normal(0, 0.5)
 				# this is a test for several jumps
 				# the t parameter is restituted to the original value
 				t = small_lambda
 
 		return x_jumps
 
-	def _merton_returns(self, x0:float, T:float, jumps:int, mu:float, rd_mu:float, rd_sigma:float, dt:float, stochastic_volatility:bool):
+	def _merton_returns(self, x0:float, T:float, jumps:int, mu:float, stochastic_volatility:bool):
 		"""
 		"""
-		geometric_brownian_motion = self._brownian_returns(x0=x0, T=T, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 													dt=dt, stochastic_volatility=stochastic_volatility)
+		geometric_brownian_motion = self._brownian_returns(x0=x0, T=T, mu=mu, stochastic_volatility=stochastic_volatility)
 
 		jump_diffusion = self._jumps_diffusion(lam=jumps)
 
 		return np.add(geometric_brownian_motion, jump_diffusion)
 
-	def merton_prices(self, jumps:int=5, mu:float=1, rd_mu:float=0, rd_sigma:float=1, stochastic_volatility:bool=False):
+	def merton_prices(self, jumps:int=5, mu:float=1, stochastic_volatility:bool=False):
 		"""
 		"""
-		dt = lam = 1 / self.T
 		_data = np.empty((self.T, self.n))
 
 		if self.n > 1:
 			for i in range(self.n):
-				_data[:, i] = np.exp(self._merton_returns(x0=np.log(self.s0), T=self.T, jumps=jumps, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+				_data[:, i] = np.exp(self._merton_returns(x0=np.log(self.s0), T=self.T, jumps=jumps, mu=mu, stochastic_volatility=stochastic_volatility))
 		else:
-			_data = np.exp(self._merton_returns(x0=np.log(self.s0), T=self.T, jumps=jumps, mu=mu, rd_mu=rd_mu, rd_sigma=rd_sigma,
-		 							  dt=dt, stochastic_volatility=stochastic_volatility))
+			_data = np.exp(self._merton_returns(x0=np.log(self.s0), T=self.T, jumps=jumps, mu=mu, stochastic_volatility=stochastic_volatility))
 
 		return _data
 
@@ -226,67 +231,174 @@ class SimPaths(object):
 	# Heston Stochastic Volatility Process (Heston Model)
 	# -------------------------------
 
+	################## special volatility for Heston model ##################
+
+	def _heston_vol_discrete(self, kappa:float, lamba:float, rt:float, rd:float, t:int):
+		# consider the process Yt = √Xt
+		dt = t / self.T
+		sigma = self.rd_sigma / 100
+
+		return kappa * ( lamba - rt ) * dt + sigma * np.power(rt*dt, 0.5) * rd
+
+	def _heston_vol_returns(self, x0:float, T:int, kappa:float, lamba:float, rd:float, t:int):
+		"""
+		"""
+		X = np.zeros(T)
+		X[0] = x0
+
+		for t in range(1, T):
+			X[t] = X[t-1] + self._heston_vol_discrete(kappa=kappa, lamba=lamba, rt=X[t-1], rd=rd[t-1], t=t)
+
+		return X
+
+	def _heston_vol_rates(self, kappa:float, lamba:float, rd:np.array, t:int=1):
+		"""
+		"""
+		_data = np.zeros((self.T, self.n))
+
+		if self.n > 1:
+			for i in range(self.n):
+				_data[:, i] = self._heston_vol_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, rd=rd, t=t)
+
+		else:
+			_data = self._heston_vol_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, rd=rd, t=t)
+
+		return _data
+
+	################## Actual Stochastic Differential model ##################
+
+	def _heston_discrete(self, mu:float, rd:float, st:float, wt:float):
+		"""
+		"""
+		# print(f'mu={mu} | rd={rd} | st{st} | wt={wt}')
+		return ( mu * st * self.dt ) + ( np.power( rd * self.dt, 0.5) * st * wt ) 
+
+	def _heston_returns(self, x0:float, T:int, mu:float, kappa:float, lamba:float, rho:float):
+		"""
+		"""
+		# integrate a random correlation level
+		dw1, dw2 = self._corr_noise(T=T, rho=rho)
+
+		# integrating into the mean reverting volatility
+		cir_vol = self._heston_vol_rates(kappa=kappa, lamba=lamba, rd=dw1).reshape(-1, 1)
+
+		X = np.zeros(T)
+		X[0] = x0
+
+		# building the stochastic process for the discretized increments
+		for t in range(1, T):
+			X[t] = X[t-1] + self._heston_discrete(mu=mu, rd=cir_vol[t-1], st=X[t-1], wt=dw2[t-1])
+
+		return X
+
+	def heston_prices(self, mu:float, kappa:float, lamba:float, rho:float):
+		"""
+		"""
+		_data = np.zeros((self.T, self.n))
+
+		if self.n > 1:
+			for i in range(self.n):
+				_data[:, i] = self._heston_returns(x0=self.s0, T=self.T, mu=mu, kappa=kappa, lamba=lamba, rho=rho)
+
+		else:
+			_data = self._heston_returns(x0=self.s0, T=self.T, mu=mu, kappa=kappa, lamba=lamba, rho=rho)
+
+		return _data
+
 	# -------------------------------
 	# Cox Ingersoll Ross (CIR) stochastic proces
 	# -------------------------------
 
-	def _cir_continuos(self, ):
+	def _cir_discrete(self, kappa:float, lamba:float, rt:float, rd:float, t:int):
 		"""
 		The Cox-Ingersoll-Ross model (CIR) is a mathematical 
 		formula used to model interest rate movements and is 
 		driven by a sole source of market risk. It is used as 
 		a method to forecast interest rates and is based on a
 		stochastic differential equation.
-
 		KEY TAKEAWAYS
-
 		The CIR is used to forecast interest rates.
 		The CIR is a one-factor equilibrium model that uses a
-		 square-root diffusion process to ensure that the 
-		 calculated interest rates are always non-negative.
+		square-root diffusion process to ensure that the 
+		calculated interest rates are always non-negative.
+		source: https://en.wikipedia.org/wiki/Cox%E2%80%93Ingersoll%E2%80%93Ross_model
+		https://www.jstor.org/stable/3532742?read-now=1&refreqid=excelsior%3A777958c5a8c04e6c69b3dfa464a1844a&seq=1#page_scan_tab_contents
 		"""
-		return
+		# consider the process Yt = √Xt
+		dt = t / self.T
+		sigma = self.rd_sigma / 100
+
+		return kappa * ( lamba - rt ) * dt + sigma * np.power(rt*dt, 0.5) * rd
+
+	def _cir_returns(self, x0:float, T:int, kappa:float, lamba:float, t:int, stochastic_volatility:bool):
+		"""
+		"""
+		X = np.zeros(T)
+		X[0] = x0
+
+		for t in range(1, T): 
+			_rd = self._random_disturbance(stochastic_volatility=stochastic_volatility)
+			X[t] = X[t-1] + self._cir_discrete(kappa=kappa, lamba=lamba, rt=X[t-1], rd=_rd, t=t)
+
+		return X
+
+	def cir_rates(self, kappa:float, lamba:float, t:int=10, stochastic_volatility:bool=False):
+		"""
+		"""
+		_data = np.zeros((self.T, self.n))
+
+		if self.n > 1:
+			for i in range(self.n):
+				_data[:, i] = self._cir_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, 
+					t=t, stochastic_volatility=stochastic_volatility)
+
+		else:
+			_data = self._cir_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, 
+				t=t, stochastic_volatility=stochastic_volatility)
+
+		return _data
 
 	# -------------------------------
 	# The Ornstein-Uhlenbech Mean Reverting Model
 	# -------------------------------
 
-	def _ou_continuos(self, rho:float, mu:float, st:float, rd:float, dt:float):
+	def _ou_discrete(self, st:float, rho:float, mu:float, rd:float):
 		"""
 		"""
-		return rho * ( mu - st ) * dt + ( rd * np.sqrt(dt) )
-
-	def _ou_returns(self, s0:float, T:int, rho:float, mu:float, rd_mu:float, rd_sigma:float, dt:float, stochastic_volatility:bool=False):
-		"""
-		"""
-		# preallocate the data
-		dS = np.zeros(T)
-		dS[0] = s0
-		for t in range(1, T):
-			rd = self._random_disturbance(mu=rd_mu, sigma=rd_sigma, stochastic_volatility=stochastic_volatility)
-			dS[t] = self._ou_continuos(rho=rho, mu=mu, st= dS[t-1], rd=rd, dt=dt)
-
-		return dS
-
-	def _ou_discrete(self, st:float, rho:float, mu:float, rd:float, dt:float):
-		"""
-		"""
-		exp_lambda = np.exp(-rho * dt)
-		sigma_lambda = ( ( 1 - np.exp(-2 * rho * dt) ) / ( 2 * rho ) ) ** 0.5
+		exp_lambda = np.exp(-rho * self.dt)
+		sigma_lambda = ( ( 1 - np.exp(-2 * rho * self.dt) ) / ( 2 * rho ) ) ** 0.5
 
 		return exp_lambda * st + ( 1 - exp_lambda ) * mu + sigma_lambda * rd
 
-	def _ou_prices(self, s0:float, T:int, rho:float, mu:float, rd_mu:float, rd_sigma:float, dt:float, stochastic_volatility:bool=False):
+	def _ou_prices(self, rho:float, mu:float, stochastic_volatility:bool=False):
 		"""
 		"""
 		# preallocate the data
-		S = np.zeros(T)
-		S[0] = s0
-		for t in range(1, T):
-			_rd = self._random_disturbance(mu=rd_mu, sigma=rd_sigma, stochastic_volatility=stochastic_volatility)
-			S[t] = self._ou_discrete(rho=rho, mu=mu, rd=_rd, dt=dt, st=S[t-1])
+		S = np.zeros(self.T)
+		S[0] = self.s0
+		for t in range(1, self.T):
+			_rd = self._random_disturbance(stochastic_volatility=stochastic_volatility)
+			_temp = self._ou_discrete(st=S[t-1], rho=rho, mu=mu, rd=_rd)
+			#case handling for negative prices - needs external assessment!
+			if _temp < 0: 
+				S[t] = 0 #np.maximum(0, np.abs(_rd + np.abs(S[t-1])))
+			S[t] = _temp
 
 		return S
+
+	def ou_prices(self, rho:float=0.2, mu:float=0.5, stochastic_volatility:bool=False):
+		"""
+		"""
+		_data = np.zeros((self.T, self.n))
+
+		if self.n > 1: 
+			for i in range(self.n):
+				_data[:, i] = self._ou_prices(rho=rho, mu=mu, stochastic_volatility=stochastic_volatility)
+
+		else:
+			_data = self._ou_prices(rho=rho, mu=mu, stochastic_volatility=stochastic_volatility)
+
+		return _data
 
 	# -------------------------------
 	# Hull-White Model
@@ -295,6 +407,42 @@ class SimPaths(object):
 	# -------------------------------
 	# Vasicek Interest Rate Model
 	# -------------------------------
+
+	def _vas_discrete(self, kappa:float, lamba:float, rt:float, rd:float, t:int):
+		"""
+		"""
+		dt = t / self.T
+		sigma = self.rd_sigma / 100
+
+		return kappa * ( lamba - rt ) * dt +  sigma * np.power(dt, 0.5) * rd
+
+	def _vas_returns (self, x0:float, T:int, kappa:float, lamba:float, t:int, stochastic_volatility:bool):
+		"""
+		"""
+		X = np.zeros(T)
+		X[0] = x0
+
+		for t in range(1, T): 
+			_rd = self._random_disturbance(stochastic_volatility=stochastic_volatility)
+			X[t] = X[t-1] + self._vas_discrete(kappa=kappa, lamba=lamba, rt=X[t-1], rd=_rd, t=t)
+
+		return X
+
+	def vas_rates(self, kappa:float, lamba:float, t:int=10, stochastic_volatility:bool=False):
+		"""
+		"""
+		_data = np.zeros((self.T, self.n))
+
+		if self.n > 1:
+			for i in range(self.n):
+				_data[:, i] = self._vas_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, 
+					t=t, stochastic_volatility=stochastic_volatility)
+
+		else:
+			_data = self._vas_returns(x0=self.r0, T=self.T, kappa=kappa, lamba=lamba, 
+						t=t, stochastic_volatility=stochastic_volatility)
+
+		return _data
 
 	# -------------------------------
 	# Heath-Jarrow-Morton Model
--- code/simulation.py
+++ code/simulation.py
@@ -23,16 +23,20 @@ if __name__=='__main__':
 	# # generate synthetic data
 	start_fake_data = time.time()
 
+	sims = SimPaths(n=500, T=252, s0=100)
+
 	browmians = sims.brownian_prices()
 	gbms = sims.gbm_prices()
 	mertons = sims.merton_prices()
+	ous = sims.ou_prices(mu=100)
+	hestons = sims.heston_prices(mu=1, kappa=0.2, lamba=0.02, rho=0.85)
 
-	_sim = np.hstack((browmians, gbms))
-	sim_prices = np.hstack((_sim, mertons))
+	# stacking all prices in just one matrix
+	prices = np.hstack((browmians, gbms, mertons, ous, hestons))
 
 	# Measuring time
 	end_fake_data = time.time()
-	print(f'It took {end_fake_data - start_fake_data} seconds')
+	print(f'It took {end_fake_data - start_fake_data} seconds generate synthetic data')
 
 	# check the priori results for different ranges
 	vrt_visuals = VRTVisuals()
@@ -43,7 +47,7 @@ if __name__=='__main__':
 	q = 5
 	print('\nMarket simulated prices')
 	# plot the head of the numpy matrix
-	print(sim_prices.shape)
+	print(prices.shape)
 	# generate the asymtotic values - to compare the stadistics against the real
 	# values of a distribuition
 	dist_values = np.random.normal(0, 1, tickers)
@@ -53,14 +57,14 @@ if __name__=='__main__':
 	z_values = []
 	p_values = []
 	for i in range(tickers):
-		_z, _p = emh.vrt(sim_prices[:, i], q, heteroskedastic=True)
+		_z, _p = emh.vrt(prices[:, i], q, heteroskedastic=True)
 		z_values.append(_z)
 		p_values.append(_p)
 
 	end_vrt = time.time()
 	print('It took %.4f seconds to calculate the statistics for %s tickers' % (end_vrt - start_vrt, tickers))
 	# check the cetral measures for the z-values
-	print(f'The mean values for the z-values is {mean(z_values)}')
+	print(f'The mean values for the p-values is {mean(p_values)}')
 
 	# plot the results obtained on simulated asset prices
 	vrt_visuals.densities(dist_values, z_values)
