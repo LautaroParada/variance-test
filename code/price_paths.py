889355cb00f227864b8d06a848df18a6911aeb0f
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-02-08 11:58:45 -0300

    Vasicek model
    Implementation of the Vasicek model.

--- code/price_paths.py
+++ code/price_paths.py
@@ -5,11 +5,13 @@ class PricePaths(object):
     
     def __init__(self, n:int, T:int, s0:float):
         
-        self.n = n      # number of paths to generate
-        self.T = T      # number of observations to generate
-        self.s0 = s0    # initial price
+        self.n = n                      # number of paths to generate
+        self.T = T                      # number of observations to generate
+        self.s0 = s0                    # initial price
         
-        self.h = self.n / self.T
+        self.h = self.n / self.T        # step to move in each step
+        self.r0 = self.s0 / 100         # Initial rate, based on the price
+        
 
 	# -------------------------------------------
 	# The Brownian Motion Stochastic Process (Wiener Process)
@@ -68,10 +70,43 @@ class PricePaths(object):
             mert_prices = self.__merton_returns(mu, sigma, lambda_, sto_vol)
         
         return mert_prices
+    
+	# -------------------------------------------
+	# Vasicek Interest Rate Model
+	# -------------------------------------------
+    
+    def vas_rates(self, mu:float, sigma:float, lambda_:float, sto_vol:bool=False):
+        
+        ou_rates = self.__zeros()
+        if self.n > 1:
+            for i in range(self.n):
+                ou_rates[:, i] = self.__vas_returns(mu, sigma, lambda_, sto_vol)
+        
+        else:
+            ou_rates = self.__vas_returns(mu, sigma, lambda_, sto_vol)
+        
+        return ou_rates
 
 	# -------------------------------------------
 	# Helper methods
 	# -------------------------------------------
+    
+    # Vasicek Interest Rate Model
+    
+    def __vas_discrete(self, mu:float, sigma:float, lambda_:float, rt:float, vol:float):
+        
+        return lambda_ * (mu-rt) * self.h + sigma * np.sqrt(self.h) * vol
+    
+    def __vas_returns(self, mu:float, sigma:float, lambda_:float, sto_vol:float):
+        volatility = self.__random_disturbance(sto_vol, rd_mu=mu, rd_sigma=sigma)
+        vas_rets = np.zeros((self.T))
+        vas_rets[0] = self.r0
+        
+        for t in range(1, self.T):
+            vas_rets[t] = vas_rets[t-1] + self.__vas_discrete(mu=mu, sigma=sigma, lambda_=lambda_, rt=vas_rets[t-1], vol=volatility[t])
+            
+        return vas_rets
+        
     
     # Merton Jump Diffusion Stochastic Process
     
@@ -134,21 +169,22 @@ class PricePaths(object):
     def __zeros(self):
         return np.zeros((self.T, self.n))
 
+#%% Run the simulation
 
 if __name__=='__main__':
     
     import matplotlib.pyplot as plt
     import seaborn as sns
     
-    n = 1
-    T = 1000
-    s0 = 1
+    n = 1                   # number of time series to simulate
+    T = 1000                # number of steps
+    s0 = 1                  # Initial price or initial short term rate
     
     sim = PricePaths(n, T, s0)
     
-    mu = 0.05
-    sigma = 0.05
-    lam = 100
+    mu = 0.05               # Long term mean return
+    sigma = 0.05            # Volatility
+    lam = 100               # Intensity of the Jump (Merton process)
     
     bro = sim.brownian_prices(mu, sigma)
     gbm = sim.gbm_prices(mu, sigma)
@@ -159,11 +195,34 @@ if __name__=='__main__':
     plt.title('Correlation across the simulated instruments')
     plt.show()
     
+    # Price plots
     plt.plot(bro, label='Brownian')
     plt.plot(gbm, label='GBM')
     plt.plot(merton, label='Merton')
     plt.title('Simulated price paths')
     plt.ylabel('price')
+    plt.xlabel('step')
+    plt.legend()
+    plt.show()
+    
+    # Rate plots
+    
+    n = 1                   # number of time series to simulate
+    T = 1000                # number of steps
+    r0 = 3.0                # Initial short term rate
+    
+    sim = PricePaths(n, T, r0)
+    
+    mu = r0/100             # Long term rate %
+    sigma = 0.01            # Volatility %
+    lam = 0.7               # Reversion speed -> 0 <= lambda <= 1
+    
+    vas = sim.vas_rates(mu, sigma, lambda_=lam)
+    
+    plt.plot(vas, label='Vasicek')
+    plt.plot(np.ones(sim.T) * mu, color='red')
+    plt.title('Simulated rate paths')
+    plt.ylabel('rate')
     plt.xlabel('step')
     plt.legend()
     plt.show()
