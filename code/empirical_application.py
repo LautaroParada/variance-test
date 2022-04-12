c786472659f03a8c7b96258db53691d721f08a2d
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-04-12 10:33:44 -0400

    Archivos para evaluar
    actualizaer ek codigo (si es necesario de los archivos) para luego crear un package del test

--- code/empirical_application.py
+++ code/empirical_application.py
@@ -8,10 +8,9 @@ Created on Mon Feb 15 10:32:33 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import time
 from datetime import datetime
 import os
-from polygon import RESTClient
+from eod import EodHistoricalData
 
 from price_paths import PricePaths
 from visuals import VRTVisuals
@@ -19,49 +18,14 @@ from variance_test import EMH
 
 #%% Creating the reference variables
 
-api_key = os.environ['API_POLY']
-client = RESTClient(api_key)
+api_key = os.environ['API_EOD']
+client = EodHistoricalData(api_key)
 
-base = 'XRP'
-quote = 'USD'
-date_ = datetime.today().strftime("%Y-%m-%d")
-
-#%% Requesting the data for the selected instruments
-
-def tick_extractor(req):
-    """
-    Convert the request into a structured pandas df
-
-    Parameters
-    ----------
-    req : model responso from Polygon
-
-    Returns
-    -------
-    pd.Dataframe
-
-    """
-
-    return pd.DataFrame(
-        data={
-            'prices':       [ req.ticks[i]['p'] for i in range(len(req.ticks)) ],
-            'sizes':        [ req.ticks[i]['s'] for i in range(len(req.ticks)) ],
-            'timestamp':    [ req.ticks[i]['t'] for i in range(len(req.ticks)) ],
-            'exchange':     [ req.ticks[i]['x'] for i in range(len(req.ticks)) ],
-            'conditions':   [ req.ticks[i]['c'] for i in range(len(req.ticks)) ]
-            }
-        )
-
-resp = client.crypto_historic_crypto_trades(base, quote, date_, limit=1000)
-test = tick_extractor(req=resp)
-plt.plot(test.iloc[:, 0])
-
-plt.show()
+stock_prices = pd.DataFrame(
+    client.get_prices_eod('SPY.US', period='d')
+    )
 
 #%% Testting the VRT
 emh = EMH()                     # Initialization of the test class
-q = 5
-z, p = emh.vrt(X=test.iloc[:, 0].values, q=q, heteroskedastic=True)
-#%%
-resp = client.stocks_equities_aggregates('X:BTCUSD', 3, 'minute', 
-                                         from_='2020-10-14', to='2021-02-14', limit=50000)
+q = 200
+z, p = emh.vrt(X=stock_prices['close'].values, q=q, heteroskedastic=True)
