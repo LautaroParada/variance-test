4d5be36e59c3f6a6a6a67a0ad70fdb5ba063efce
LautaroParada <lautaro.parada.opazo@gmail.com>
2023-04-07 02:31:36 -0400

    gbm_prices
    Esta versión de gbm_prices añade una validación para los parámetros mu y sigma antes de realizar los cálculos. Si mu y/o sigma no son números, se lanza un error ValueError con un mensaje descriptivo. Además, se han agregado docstrings y comentarios en el código para facilitar su comprensión y mantenimiento.

--- code/price_paths.py
+++ code/price_paths.py
@@ -52,12 +52,43 @@ class PricePaths(object):
             prices[t] = prices[t - 1] * np.exp(brownian_increments[t - 1])
 
         return prices
-        
+
 	# -------------------------------------------
 	# Geometric Brownian motion
 	# -------------------------------------------
     
-    
+    def gbm_prices(self, mu, sigma):
+        """
+        Genera una simulación de precios siguiendo un proceso de movimiento browniano geométrico (GBM).
+
+        Parámetros:
+        mu (float): Tasa de rendimiento esperado de la acción.
+        sigma (float): Volatilidad de la acción.
+
+        Retorna:
+        numpy.ndarray: Array 1D de precios simulados.
+        """
+
+        # Validación de parámetros
+        if not (isinstance(mu, (int, float)) and isinstance(sigma, (int, float))):
+            raise ValueError("Los parámetros mu y sigma deben ser números")
+
+        # Inicialización de los precios con el precio inicial
+        prices = np.zeros(self.n + 1)
+        prices[0] = self.s0
+
+        # Generación de incrementos brownianos
+        brownian_increments = np.random.normal(
+            loc=0,
+            scale=np.sqrt(self.dt),
+            size=self.n
+        )
+
+        # Cálculo de precios utilizando GBM
+        for t in range(1, self.n + 1):
+            prices[t] = prices[t - 1] * np.exp((mu - 0.5 * sigma**2) * self.dt + sigma * brownian_increments[t - 1])
+
+        return prices  
     
 	# -------------------------------------------
 	# Merton Jump Diffusion Stochastic Process
