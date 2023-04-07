230129183659b4aa9dd768b209256750bf8a025a
LautaroParada <lautaro.parada.opazo@gmail.com>
2023-04-07 03:42:45 -0400

    De vuelta a la versión original

--- code/price_paths.py
+++ code/price_paths.py
@@ -4,125 +4,59 @@ from scipy.stats import halfnorm
 class PricePaths(object):
     
     def __init__(self, n:int, T:int, s0:float):
-
-        if n <= 0 or T <= 0 or s0 <= 0:
-            raise ValueError("Los parámetros n, T y s0 deben ser números positivos")
-                
+        
         self.n = n                      # number of paths to generate
         self.T = T                      # number of observations to generate
         self.s0 = s0                    # initial price
         
         self.h = self.n / self.T        # step to move in each step
-        self.dt = self.h
         self.r0 = self.s0 / 100         # Initial rate, based on the price
         
+
+    # -------------------------------------------
+    # The Brownian Motion Stochastic Process (Wiener Process)
+    # -------------------------------------------
+    
+    def brownian_prices(self, mu:float, sigma:float):
+        
+        # preallocate the data
+        bro_prices = self.__zeros()
+        
+        # check the size of the output matrix
+        if self.n > 1:
+            for i in range(self.n):
+                # simulate n price paths
+                bro_prices[:, i] = self.__brownian_returns(mu, sigma, sto_vol=True)
+        else:
+            # case for only 1 simulation
+            bro_prices = self.__brownian_returns(mu, sigma, sto_vol=True)
+            
+        return bro_prices
+    
     # -------------------------------------------
     # Geometric Brownian motion
     # -------------------------------------------
     
-    def brownian_prices(self, mu, sigma):
-        """
-        Genera una simulación de precios siguiendo un movimiento browniano geométrico.
+    def gbm_prices(self, mu:float, sigma:float):
+        # preallocate the data
+        gbm_prices = self.__zeros()
 
-        Parámetros:
-        mu (float): Tasa de rendimiento esperado de la acción.
-        sigma (float): Volatilidad de la acción.
+        # check the size of the output matrix
+        if self.n > 1:
+            for i in range(self.n):
+                # simulate n price paths
+                gbm_prices[:, i] = self.__brownian_returns(mu, sigma, sto_vol=False)
+        else:
+            # case for only 1 simulation
+            gbm_prices = self.__brownian_returns(mu, sigma, sto_vol=False)
 
-        Retorna:
-        numpy.ndarray: Array 1D de precios simulados.
-        """
-
-        # Validación de parámetros
-        if not (isinstance(mu, (int, float)) and isinstance(sigma, (int, float))):
-            raise ValueError("Los parámetros mu y sigma deben ser números")
-
-        # Inicialización de los precios con el precio inicial
-        prices = np.zeros(self.n + 1)
-        prices[0] = self.s0
-
-        # Generación de incrementos brownianos
-        brownian_increments = np.random.normal(
-            loc=(mu - 0.5 * sigma**2) * self.dt,
-            scale=np.sqrt(self.dt) * sigma,
-            size=self.n
-        )
-
-        # Cálculo de precios
-        for t in range(1, self.n + 1):
-            prices[t] = prices[t - 1] * np.exp(brownian_increments[t - 1])
-
-        return prices
-
-    # -------------------------------------------
-    # Geometric Brownian motion
-    # -------------------------------------------
-    
-    def gbm_prices(self, mu, sigma):
-        """
-        Genera una simulación de precios siguiendo un proceso de movimiento browniano geométrico (GBM).
-
-        Parámetros:
-        mu (float): Tasa de rendimiento esperado de la acción.
-        sigma (float): Volatilidad de la acción.
-
-        Retorna:
-        numpy.ndarray: Array 1D de precios simulados.
-        """
-
-        # Validación de parámetros
-        if not (isinstance(mu, (int, float)) and isinstance(sigma, (int, float))):
-            raise ValueError("Los parámetros mu y sigma deben ser números")
-
-        # Inicialización de los precios con el precio inicial
-        prices = np.zeros(self.n + 1)
-        prices[0] = self.s0
-
-        # Generación de incrementos brownianos
-        brownian_increments = np.random.normal(
-            loc=0,
-            scale=np.sqrt(self.dt),
-            size=self.n
-        )
-
-        # Cálculo de precios utilizando GBM
-        for t in range(1, self.n + 1):
-            prices[t] = prices[t - 1] * np.exp((mu - 0.5 * sigma**2) * self.dt + sigma * brownian_increments[t - 1])
-
-        return prices
+        return gbm_prices
     
     # -------------------------------------------
     # Merton Jump Diffusion Stochastic Process
     # -------------------------------------------
     
-    def merton_prices(self, mu:float, sigma:float, lambda_:int):
-        """
-        This model superimposes a jump component on a diffusion component.
-        The diffusion component is the familiar geometric Brownian motion.
-        The jump component is composed of lognormal jumps driven by a Poisson
-        process.
-            - It models the sudden changes in the stock price because of the 
-            arrival of important new information.
-            - The lognormal jumps, and the Poisson process are assumed to
-            be independent.
-        
-        The jump event is governed by a compound Poisson process qt with 
-        intensity λ, where k denotes the magnitude of the random jump.
-
-        Parameters
-        ----------
-        mu : float
-            mean returns for the instrument (first moment).
-        sigma : float
-            volatility of the instrument (second moment).
-        lambda_ : int
-            intensity/frequency of the jump.
-
-        Returns
-        -------
-        mert_prices : numy array
-            prices with at least a jump.
-
-        """
+    def merton_prices(self, mu:float, sigma:float, lambda_:int, sto_vol:bool=False):
         # preallocate the data
         mert_prices = self.__zeros()
         
@@ -130,10 +64,10 @@ class PricePaths(object):
         if self.n > 1:
             for i in range(self.n):
                 # simulate n price paths
-                mert_prices[:, i] = self.__merton_returns(mu, sigma, lambda_, sto_vol=False)
+                mert_prices[:, i] = self.__merton_returns(mu, sigma, lambda_, sto_vol)
         else:
             # case for only 1 simulation
-            mert_prices = self.__merton_returns(mu, sigma, lambda_, sto_vol=False)
+            mert_prices = self.__merton_returns(mu, sigma, lambda_, sto_vol)
         
         return mert_prices
     
@@ -168,23 +102,23 @@ class PricePaths(object):
         
         return cir_rates_
     
-   # -------------------------------------------
+    # -------------------------------------------
     # Heston Stochastic Volatility Process
     # -------------------------------------------
     
-    def heston_prices(self, mu:float, k:float, theta:float, sigma:float):
+    def heston_prices(self, rf:float, k:float, theta:float, sigma:float, sto_vol:bool=False):
         hes_prices = self.__zeros()
         
         if self.n > 1:
             for i in range(self.n):
-                hes_prices[:, i] = self.__heston_returns(mu, k, theta, sigma)
+                hes_prices[:, i] = self.__heston_returns(rf, k, theta, sigma, sto_vol)
                 
         else:
-            hes_prices = self.__heston_returns(mu, k, theta, sigma)
+            hes_prices = self.__heston_returns(rf, k, theta, sigma, sto_vol)
             
         return hes_prices
     
-   # -------------------------------------------
+    # -------------------------------------------
     # Ornstein–Uhlenbeck Process (Mean reverting)
     # -------------------------------------------
     
@@ -222,36 +156,37 @@ class PricePaths(object):
     
     def __heston_dis_vol(self, k:float, theta:float, vt:float, sigma:float, w2:float):
         # heston mean reverting volatility recurrence
-        return k * (sigma - np.abs(vt)) * self.h + theta * np.sqrt(np.abs(vt) * self.h) * w2
+        return k * (theta - vt) * self.h + sigma * np.sqrt(np.abs(vt) * self.h) * w2
     
-    def __heston_discrete(self, mu:float, st:float, V, w1):
+    def __heston_discrete(self, rf:float, st:float, V, w1):
         # Discrete form of the Heston model
-        return mu * st * self.h + np.sqrt(np.abs(V)) * st * np.sqrt(self.h) * w1
+        return rf * st *self.h + np.sqrt(np.abs(V) * self.h) * st * w1
     
-    def __heston_returns(self, mu:float, k:float, theta:float, sigma:float):
+    def __heston_returns(self, rf:float, k:float, theta:float, sigma:float, sto_vol:bool):
         
         # integrate a random correlation level
         corr_wn1, corr_wn2 = self.__corr_noise()
         
-        # integrating the mean reverting volatility of two Wiener processes
+        # integrating the mean reverting volatility
+        wn2 = self.__random_disturbance(sto_vol=sto_vol, rd_mu=0, rd_sigma=0)
         dw2 = np.zeros(self.T)
         dw2[0] = corr_wn2[0]
         
         for t in range(1, self.T):
-            dw2[t] = corr_wn2[t-1] + self.__heston_dis_vol(k=k, 
-                                                       theta=theta, 
-                                                       vt=dw2[t-1], 
-                                                       sigma=sigma, 
-                                                       w2=corr_wn2[t])
+            dw2[t] = self.__heston_dis_vol(k=k, 
+                                           theta=theta, 
+                                           vt=corr_wn2[t], 
+                                           sigma=sigma, 
+                                           w2=wn2[t])
             
         # creating the actual data for the process
         heston_ret = np.zeros(self.T)
         heston_ret[0] = self.s0
         
         for t in range(1, self.T):
-            heston_ret[t] = heston_ret[t-1] + self.__heston_discrete(mu=mu,
+            heston_ret[t] = heston_ret[t-1] + self.__heston_discrete(rf=rf, 
                                                                      st=heston_ret[t-1], 
-                                                                     V=dw2[t-1], 
+                                                                     V=dw2[t], 
                                                                      w1=corr_wn1[t])
             
         return heston_ret.ravel()
@@ -295,17 +230,18 @@ class PricePaths(object):
         t = 0
         jumps = np.zeros((self.T, 1))
         lambda__ = lambda_ / self.T
-        small_lambda = -(1.0/lambda__) # k or magnitude of the jump
-        pd_ = np.random.poisson(lam=lambda__, size=(self.T))
+        small_lambda = -(1.0/lambda__)
+        pd = np.random.poisson(lam=lambda__, size=(self.T))
         
         # applying the psudo-code of the algorithm
         for i in range(self.T):
             t += small_lambda * np.log(np.random.uniform())
-            if t >= self.T:
-                jumps[i:] = ( (np.mean(pd_) + np.std(pd_)) * np.random.uniform() ) * np.random.choice([-1, 1])
+            if t > self.T:
+                jumps[i:] = ( (np.mean(pd) + np.std(pd)) * np.random.uniform() ) * np.random.choice([-1, 1])
                 # the t parameter is restituted to the original value
                 # for several jumps in the future
-                t = 0
+                t = small_lambda
+                break
                 
         return jumps.reshape(-1, 1)
                 
@@ -337,10 +273,11 @@ class PricePaths(object):
     
     def __random_disturbance(self, sto_vol:bool, rd_mu:float, rd_sigma:float):
         
-        # negative volatility doesnt make sense
         if not sto_vol:
             return np.random.normal(size=(self.T, 1))
         else:
+            # error handling for scale < 0, because negative volatilities 
+            # doesnt makes sense.
             return np.random.normal(loc=rd_mu * self.h,
                                     scale=rd_sigma * halfnorm.rvs(1) * np.sqrt(self.h),
                                     size=(self.T, 1)) * 100
@@ -350,15 +287,19 @@ class PricePaths(object):
         return np.zeros((self.T, self.n))
     
     def __corr_noise(self):
-        #fuente https://lup.lub.lu.se/luur/download?func=downloadFile&recordOId=1436827&fileOId=1646914
         
         # generate two uncorrelated Brownian processes
         z1 = self.__random_disturbance(sto_vol=False, rd_mu=0, rd_sigma=1)
         z2 = self.__random_disturbance(sto_vol=False, rd_mu=0, rd_sigma=1)
         
-        # randomly create a correlation coeficient
-        rho = np.random.uniform(low=0.5, high=0.7) * np.random.choice([-1, 1])
+        # randomly create an absolute correlation
+        rho = np.random.uniform(low=0.5, high=1)
         
-        z_tV = ( rho * z1 ) + ( np.sqrt( 1 - rho**2 ) * z2 )
+        corr1 = np.sqrt( (1 + rho) / 2 )
+        corr2 = np.sqrt( (1 - rho) / 2 )
         
-        return z1, z_tV
+        # correlating the brownian processes
+        dw1 = corr1 * z1 + corr2 * z2
+        dw2 = corr1 * z1 - corr2 * z2
+        
+        return dw1, dw2
