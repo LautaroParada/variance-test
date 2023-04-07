4e6e92faac610780d6b35e570504290ee7f2277f
LautaroParada <lautaro.parada.opazo@gmail.com>
2023-04-07 02:27:37 -0400

    validación para __init__
    Esta versión de __init__ verifica que los parámetros n, T y s0 sean positivos antes de asignarlos a las variables de instancia. Si uno de los parámetros no cumple con esta condición, se lanza un error ValueError con un mensaje descriptivo. Esto ayudará a prevenir errores durante la ejecución del código y garantizará que los valores de entrada sean válidos.

--- code/price_paths.py
+++ code/price_paths.py
@@ -4,7 +4,10 @@ from scipy.stats import halfnorm
 class PricePaths(object):
     
     def __init__(self, n:int, T:int, s0:float):
-        
+
+        if n <= 0 or T <= 0 or s0 <= 0:
+            raise ValueError("Los parámetros n, T y s0 deben ser números positivos")
+                
         self.n = n                      # number of paths to generate
         self.T = T                      # number of observations to generate
         self.s0 = s0                    # initial price
